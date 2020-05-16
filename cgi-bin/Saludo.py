03e6de828fc80a408fb892b2c36c4d90ea2d9ff2
Fátima Rojas Carhuas <64380871+Fatima2427@users.noreply.github.com>
2020-05-16 07:45:23 -0500

    Update Saludo.py

--- cgi-bin/Saludo.py
+++ cgi-bin/Saludo.py
@@ -1,2 +1,12 @@
-#!/bin/python3
+#!C:\Python3\python.exe -u
+import cgi
+
+print "Content-Type: text/html\n\n";
+print '''
+<html>
+<body>
+<h1>hola</h1>
+</body>
+</html>
+print '''
 print("Hello, World!")
